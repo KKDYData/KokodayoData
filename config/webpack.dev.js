4a497f109d43370131f1ecd7ca86757753ab57e1
odex21 <odex_2100@outlook.com>
2019-06-28 00:12:51 +0800

    v0.2.9.b

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,6 +3,7 @@ const common = require('./webpack.common');
 const webpack = require('webpack');
 
 module.exports = merge(common, {
+  mode: 'development',
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
 
@@ -13,12 +14,59 @@ module.exports = merge(common, {
     contentBase: './dist',
     hot: true,
     proxy: {
-      '/api': 'http://127.0.0.1:3000',
+      '/api': 'http://127.0.0.1:8444',
     },
     host: '0.0.0.0',
     allowedHosts: [
       '192.168.31.179'
-    ]
+    ],
+    historyApiFallback: {
+      rewrites: [
+        {
+          from: /^\/$/, to: context => {
+            console.log('1:');
+            console.log(context.parsedUrl.pathname);
+            return '';
+          }
+        },
+        //详情页面
+        {
+          from: /^\/details\/(?!char).*$/, to: context => {
+            console.log('2:');
+            // console.log(context.parsedUrl);
+            console.log(context.parsedUrl.pathname.slice(9));
+            return '/' + context.parsedUrl.pathname.slice(9);
+          }
+        },
+        {
+          from: /^\/details\/(char)?/, to: context => {
+            console.log('2.1:');
+            console.log(context.parsedUrl.pathname);
+            return '/index.html';
+          }
+        },
+        {
+          from: /^\/computer$/, to: context => {
+            console.log('2.1:');
+            console.log(context.parsedUrl.pathname);
+            return '/index.html';
+          }
+        },
+
+        {
+          from: /^\/.*$/, to: context => {
+            console.log('/:');
+            console.log(context.parsedUrl.pathname);
+            return '/' + context.parsedUrl.pathname;
+          }
+        },
+        {
+          from: /./, to: context => {
+            console.log('404: ' + context.parsedUrl.pathname);
+            return '/views/error.pug';
+          }
+        }]
+    },
   },
   resolve: {
     alias: {
