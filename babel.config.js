fca3036011021325d44cdfcb7173b32b9a004458
odex21 <odex_2100@outlook.com>
2019-11-25 01:04:53 +0800

    config babel

---
+++ babel.config.js
@@ -0,0 +1,39 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage',
+        targets: {
+          esmodules: true
+        },
+        corejs: {
+          version: 3,
+          proposals: true
+        }
+      }
+    ],
+  ];
+  const plugins = [
+    [
+      '@babel/plugin-proposal-class-properties',
+    ],
+    [
+      '@babel/plugin-syntax-dynamic-import'
+    ],
+    [
+      'component',
+      {
+        'libraryName': 'element-ui',
+        'styleLibraryName': '~theme'
+      }
+    ]
+  ];
+
+  return {
+    presets,
+    plugins
+  };
+};
