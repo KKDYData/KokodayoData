804d29cc98ef809230169c28e9c79d8614173688
zhiming <wuzhiming@tuzhanai.com>
2020-05-01 19:56:23 +0800

    ✨feat: 活动更新

--- src/components/Activity/main.vue
+++ src/components/Activity/main.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <div class="activity-wrapper">
-      <!-- <div class="activity-img">
+      <div class="activity-img">
         <r-image :preview-src-list="[activityPic]" :src="activityPic" />
-      </div>-->
+      </div>
       <!-- <div class="activity-title">午间逸话</div> -->
       <!-- <div class="activity-content">
         <div class="activity-info-need">SA-1 → 6 主要掉落如下</div>
@@ -23,6 +23,19 @@
         </div>
       </div>-->
       <div style="margin-top: 20px">
+        <span
+          class="activity-info-need"
+        >不知不觉就一周年了，而kkdy到5月底的时候也一运营一年了。在这里简单重申下，这是我个人的前/后端学习项目，同时也是非盈利性质的。目前月活跃用户数也接近10万，大家的反馈，以及我不断学了新技术之后想找地方写demo，是我更新的最大动力。</span>
+      </div>
+      <div style="margin-top: 20px">
+        <p class="activity-info-need">
+          <span>近期计划</span>
+        </p>
+        <p class="activity-info-need">1. 小程序开发。（看中了小程序云开发送的免费资源，但是跨平台难度也有点大，不会太快搞出来）</p>
+        <p class="activity-info-need">2. 地图更换渲染引擎以获取更好的展现效果和性能</p>
+        <p class="activity-info-need">3. 希望继续优化网页的UI，以及支持暗色主题，如果有设计师小伙伴感兴趣，欢迎来联系我。</p>
+      </div>
+      <div style="margin-top: 20px">
         <span class="activity-info-need">版本变更为1.0版本。部分组件改为了左右滑动切换（技能详情和消耗、首页、资料和语音）</span>
       </div>
       <div style="margin-top: 20px">
@@ -40,16 +53,16 @@
 
 <script>
 // import ItemViewer from '@/components/ItemViewer'
-// import RImage from '@/components/base/RImage'
+import RImage from '@/components/base/RImage'
 
 export default {
   components: {
     // ItemViewer,
-    // RImage
+    RImage
   },
   data() {
     return {
-      activityPic: require('./img/wujianyihua.jpg')
+      activityPic: require('./img/anniversary.jpg')
     }
   }
 }
@@ -62,7 +75,7 @@ export default {
 
 .activity {
   &-content {
-    //height: 200px
+    // height: 200px
   }
 
   &-img {
@@ -114,7 +127,7 @@ export default {
     }
 
     &-content {
-      //height: vw(300)
+      // height: vw(300)
     }
 
     &-title {
