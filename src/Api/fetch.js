6f4322271f558ea4234835349e3b0a79381c8c7f
odex <odex_2100@outlook.com>
2020-05-25 02:21:15 +0800

    ✨feat(api): feat feedback

--- src/Api/fetch.js
+++ src/Api/fetch.js
@@ -2,7 +2,7 @@ import { isCrossOrigin } from './utils'
 import { memoizeWith, identity } from 'ramda'
 import { ApiError } from './error'
 
-const getMode = url => isCrossOrigin(url) ? 'cors' : 'no-cors'
+const getMode = url => isCrossOrigin(url) ? 'cors' : 'same-origin'
 const getModeC = memoizeWith(identity, getMode)
 
 export const fetchGet = (url, isApi = true) => {
@@ -25,6 +25,32 @@ export const fetchGet = (url, isApi = true) => {
       }
     } else {
       return res
+    }
+  })
+}
+
+
+export const fetchPost = (url, data) => {
+  return fetch(url, {
+    method: 'POST',
+    mode: getModeC(url),
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    cache: 'no-cache'
+  }).then(res => {
+    if (res.ok) {
+      return res.json()
+    }
+    else {
+      return new Error('net error')
+    }
+  }).then(res => {
+    if (res.ok) {
+      return res.result
+    } else {
+      throw new ApiError('server error', res)
     }
   })
 }
--- src/Api/index.js
+++ src/Api/index.js
@@ -1,12 +1,16 @@
-import { fetchGet } from './fetch'
+import { fetchGet, fetchPost } from './fetch'
 import { BASE_PATH } from './path'
 import { Assets } from './staticAssets'
-const BaseGroup = (path) => fetchGet(BASE_PATH + '/base' + path)
 
+const BaseGroup = (path) => fetchGet(BASE_PATH + '/base' + path)
 const getInfo = () => BaseGroup('/info')
 
+const YdkkGroup = (path, body) => fetchPost(BASE_PATH + '/ydkk' + path, body)
+const postFeedback = (body) => YdkkGroup('/feedback', body)
+
 const Api = {
-  getInfo
+  getInfo,
+  postFeedback
 }
 
 export {
