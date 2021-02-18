351c4548d5de8e8da790e7fa90ab7ba2e92cfe36
odex <odex_2100@outlook.com>
2021-02-18 21:18:15 +0800

    udpate Activity's content

--- src/components/Activity/main.vue
+++ src/components/Activity/main.vue
@@ -23,25 +23,34 @@
         </div>
       </div>-->
       <div style="margin-top: 20px">
-        <span
-          class="activity-info-need"
-        >Logo换新</span>
+        <span class="activity-info-need">Logo换新</span>
       </div>
       <div style="margin-top: 20px">
         <p class="activity-info-need">
-          <span>2021 1/20 近期计划:<del>开始改版 </del> 预计有两个新功能, 准备建文件夹了！</span>
+          2021 2/18 更新敌人属性的翻译，并且点击、悬浮会显示原始值
         </p>
-        <p class="activity-info-need">1. 小程序开发中...</p>
-        <p class="activity-info-need">2. 地图更换渲染引擎以获取更好的展现效果和性能</p>
-        <p class="activity-info-need">3. 如果有兴趣帮忙做下测试完善敌人/干员图鉴的内容.</p>
+        <p class="activity-info-need">
+          <span
+            >2021 1/20 近期计划:<del>开始改版 </del> 预计有两个新功能,
+            准备建文件夹了！</span
+          >
+        </p>
+        <p class="activity-info-need">1. 希望有人能帮忙重新设计下pc版</p>
+        <p class="activity-info-need">2. 小程序开发中...</p>
+        <p class="activity-info-need">
+          3. 地图更换渲染引擎以获取更好的展现效果和性能
+        </p>
       </div>
       <div style="margin-top: 20px">
         <span class="activity-info-need">
-          <router-link to="/info">点击这里</router-link>，往下滑找到“反馈”以反馈问题。
+          <router-link to="/info">点击这里</router-link
+          >，往下滑找到“反馈”以反馈问题。
         </span>
       </div>
       <div style="margin-top: 20px">
-        <span class="activity-info-need light">企鹅物流的掉落数据，同步时间为10分钟/次。</span>
+        <span class="activity-info-need light"
+          >企鹅物流的掉落数据，同步时间为10分钟/次。</span
+        >
       </div>
     </div>
   </div>
