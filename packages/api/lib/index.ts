46dcc5aeb582b7a6698203067c8da36bcd9f349c
odex <odex_2100@outlook.com>
2021-04-06 03:31:04 +0800

    chore: update api api-taro

--- packages/api/lib/index.ts
+++ packages/api/lib/index.ts
@@ -1,3 +1,4 @@
 export * as ApiData from './data'
 export * as ApiHome from './home'
 export * as ApiUser from './user'
+export * from './instance'
--- packages/apiTaro/src/instance.ts
+++ packages/apiTaro/src/instance.ts
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from 'taro-axios'
 
 export const request = axios.create({
-  baseURL: (import.meta.env.VITE_API_URL as string) ?? 'http://127.0.0.1:7001',
+  baseURL: 'https://test.api.kokodayo.fun',
   withCredentials: true,
 })
 
