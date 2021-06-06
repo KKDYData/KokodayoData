9b743243d9e8615e717f169bffcd5d765eecf5a9
odex <odex_2100@outlook.com>
2021-06-06 18:25:07 +0800

    update data

--- packages/data/lib/ActivityInfo.ts
+++ packages/data/lib/ActivityInfo.ts
@@ -3,20 +3,21 @@
 // 使用 quicktype 生成的类型，不要乱改！
 
 export interface IInfo {
-    id:             string;
-    type:           string;
-    name:           string;
-    startTime:      number;
-    endTime:        number;
-    rewardEndTime:  number;
-    displayOnHome:  boolean;
-    hasStage:       boolean;
-    actTopBarColor: null | string;
-    actTopBarText:  null | string;
-    templateShopId: null | string;
-    medalGroupId:   null | string;
-    isReplicate:    boolean;
-    displayType?:   DisplayType;
+    id:                 string;
+    type:               string;
+    name:               string;
+    startTime:          number;
+    endTime:            number;
+    rewardEndTime:      number;
+    displayOnHome:      boolean;
+    hasStage:           boolean;
+    actTopBarColor:     null | string;
+    actTopBarText:      null | string;
+    templateShopId:     null | string;
+    medalGroupId:       null | string;
+    isReplicate:        boolean;
+    displayType?:       DisplayType;
+    ungroupedMedalIds?: string[];
 }
 
 export enum DisplayType {
