b0f815713cf466fc1b8ec94ec09a2f818233bd6f
odex <odex_2100@outlook.com>
2021-06-16 22:42:40 +0800

    wip: DPS template

---
+++ packages/weapp/src/pages/index/constants.ts
@@ -0,0 +1,37 @@
+export const NavigationItems = [
+  {
+    name: '干员档案',
+  },
+  {
+    name: 'DPS计算',
+    url: '/pages/dps/dps',
+  },
+  {
+    name: '材料详情',
+  },
+  {
+    name: '后勤技能',
+  },
+  {
+    name: '关卡数据',
+  },
+  {
+    name: '公招计算',
+  },
+  {
+    name: '时装一览',
+  },
+  {
+    name: '经验计算',
+  },
+  {
+    name: '家具一览',
+  },
+
+  {
+    name: '卡池一览',
+  },
+  {
+    name: '打钱',
+  },
+]
