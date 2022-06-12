e23d40f3db3475ebcf11cb31750cef8493a7fe6a
odex <odex_2100@outlook.com>
2022-06-12 13:19:22 +0800

    feat: new components RadioButtonGroup

---
+++ packages/weapp/src/components/RadioButtonGroup/RadioButtonGroup.vue
@@ -0,0 +1,34 @@
+<template>
+  <view class="shadow-md flex rounded-2rpx">
+    <view
+      v-for="op in options"
+      :key="op.value"
+      class="radio-button"
+      :class="{ selected: op.value === modelValue }"
+      @touchend="emit('update:modelValue', op.value)"
+    >
+      {{ op.label }}
+    </view>
+  </view>
+</template>
+<script setup lang="ts">
+type ValueType = string | number | symbol
+
+defineProps<{
+  options: { label: string | number; value: ValueType }[]
+  modelValue: ValueType
+}>()
+
+const emit = defineEmits<{
+  (name: 'update:modelValue', v: ValueType): void
+}>()
+</script>
+<style lang="styl">
+.radio-button {
+  @apply text-20rpx text-dark-50 px-20rpx py-5rpx rounded-2rpx
+
+  &.selected {
+    @apply shadow-md text-white bg-dark-50
+  }
+}
+</style>
